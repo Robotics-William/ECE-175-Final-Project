aa5e3dcd192f61c43d5bd38aeafd6a4996b29d17
William Young <william.victorem@gmail.com>
2023-04-21 19:13:12 -0700

    Changes

--- Final Project/Main.cpp
+++ Final Project/Main.cpp
@@ -1,23 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include "functions.h"
 
 int main(void) {
 
 
-	printf("Hello Michael!\n");
-	printf("Hello William!\n");
-
-	int steve;
-	int patrick;
-	int cornwallis;
-	int jackson;
-
-	int x = &steve - &patrick;
-	int y = &patrick - &cornwallis;
-	int z = &cornwallis - &jackson;
-	int w = &steve - &jackson;
-
-	printf("%d %d %d %d\n", x, y, z, w);
+	
 
 
 	return 0;
---
+++ Final Project/functions.h
@@ -0,0 +1,12 @@
+#ifndef FILENAME_H
+#define FILENAME_H
+
+void readCardFile() {
+
+	printf("Hello Michael!\n");
+	printf("Hello William!\n");
+
+
+}
+
+#endif
