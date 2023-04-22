e460b1495b7f1ebfef332e724134adacd1c8a5b3
William Young <william.victorem@gmail.com>
2023-04-21 20:50:46 -0700

    Big Update

--- Final Project/Main.cpp
+++ Final Project/Main.cpp
@@ -1,6 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 #include "functions.h"
+
 //#include "cards.c"
 
 #define deckSize 5
@@ -10,9 +17,19 @@ int main(void) {
 	FILE* inputFile = NULL;
 	card deck[deckSize];
 
+	//opens file
 	inputFile = fopen("cards.txt", "r");
 
+	//reads file contents into the deck of cards
 	readCardFile(inputFile, deckSize, deck);
+
+	//suffles the deck
+	shuffleCard(deck, deckSize);
+
+	//deal a card
+	dealCard();
+
+	//prints the cards for code development 
 	printCardArray(deck, deckSize);
 
 
--- Final Project/functions.h
+++ Final Project/functions.h
@@ -1,6 +1,13 @@
 #ifndef FILENAME_H
 #define FILENAME_H
-#include "cards.c"
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
 
 typedef struct card_s {
 	char color[10];
@@ -10,16 +17,43 @@ typedef struct card_s {
 } card;
 
 void readCardFile(FILE *inp, int size, card carray[]) {
+	int i;
 
-	for (int i = 0; i < size; i++) {
+	for (i = 0; i < size; i++) {
 		fscanf(inp, "%s %d %s", carray[i].color, &carray[i].value, carray[i].action);
 	}
 
 }
 
-void printCardArray(card carray[], int size) {
+void shuffleCard(card carray[], int size) {
+	int i;
+	int a, b; 
+	card temp;
 
-	for (int i = 0; i < size; i++) {
+	srand(int(time(0)));
+
+	for (i = 0; i < 108; i++) {
+
+		a = rand() % size;
+		b = rand() % size;
+
+		temp = carray[a];
+		carray[a] = carray[b];
+		carray[b] = carray[a];
+
+	}
+
+
+
+
+}
+
+card dealCard();
+
+void printCardArray(card carray[], int size) {
+	int i;
+
+	for (i = 0; i < size; i++) {
 		printf("%s %d %s\n", carray[i].color, carray[i].value, carray[i].action);
 	}
 
