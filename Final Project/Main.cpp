b0b195f37e6a1c486a40e8f24621726bf0f6c3ae
William Young <william.victorem@gmail.com>
2023-04-21 18:59:47 -0700

    Update Main.cpp

--- Final Project/Main.cpp
+++ Final Project/Main.cpp
@@ -7,6 +7,17 @@ int main(void) {
 	printf("Hello Michael!\n");
 	printf("Hello William!\n");
 
+	int steve;
+	int patrick;
+	int cornwallis;
+	int jackson;
+
+	int x = &steve - &patrick;
+	int y = &patrick - &cornwallis;
+	int z = &cornwallis - &jackson;
+	int w = &steve - &jackson;
+
+	printf("%d %d %d %d\n", x, y, z, w);
 
 
 	return 0;
